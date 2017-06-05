a323d3c97609efa0a99ad68bf4aa29769226cb90
becci.denny <bexicles@gmail.com>
2017-06-05 12:47:30 +0100

    Added comments-skeleton to code, and data file

---
+++ DFS.py
@@ -0,0 +1,7 @@
+
+t = 0 ## global variable for finishing times
+s = None ## global variable for current source vertex
+
+## DFS_Loop function
+
+def DFS_Loop(G):
---
+++ Kosarajus_Two_Pass.py
@@ -0,0 +1,9 @@
+import DFS
+
+## Load Graph, G (map of directed vertices travelling from left number node to right number node)
+
+## Reverse Graph G, and save as G_rev
+
+## Run DFS_Loop on G_rev to find node ordering
+
+## Run DFS_Loop on G to discover SCCs one by one [SCCs are nodes with the same "leader"
